80ff5e92692786dbc8c4bdccb6272504bbc6cfd6
Ryan Hildebrant <48308793+Ryanhilde@users.noreply.github.com>
2020-10-13 14:24:21 -0700

    Update ai.py

--- ai.py
+++ ai.py
@@ -1,13 +1,3 @@
-"""
- We the undersigned promise that we have in good faith attempted to follow the principles of pair programming.
- Although we were free to discuss ideas with others, the implementation is our own. We have shared a common
- workspace and taken turns at the keyboard for the majority of the work that we are submitting.
- Furthermore, any non programming portions of the assignment were done independently. We recognize that
- should this not be the case, we will be subject to penalties as outlined in the course syllabus.
-Pair Programmer 1 - Cole Ferguson, 10/6/20
-Pair Programmer 2 - Ryan Hildebrant, 10/6/20
-"""
-
 from lib import abstractstrategy
 
 from math import inf
@@ -150,8 +140,7 @@ class Strategy(abstractstrategy.Strategy):
         Returns (newboard, action)
         """
         action = self.search.alphabeta(board)
-        print("THIS IS OUR CHOICE OF PLAY!")
-        print(action)
+        print("Red moves " + board.get_action_str(action))
         if action is None:
             return board, None
         return board.move(action), action
